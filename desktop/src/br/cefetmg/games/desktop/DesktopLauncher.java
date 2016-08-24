70194e14719482210d058dbf778860d7fb67c155
Flavio Coutinho <fegemo@gmail.com>
2016-08-24 17:26:34 -0300

    Project setup

---
+++ desktop/src/br/cefetmg/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.cefetmg.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.cefetmg.games.BrushMyTeethPlzGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BrushMyTeethPlzGame(), config);
+	}
+}
