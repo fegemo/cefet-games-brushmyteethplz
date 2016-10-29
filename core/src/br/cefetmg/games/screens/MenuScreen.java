d157edbe9e2ed7c78484940fbb9eb830161e6556
Carlos Henrique Alvarenga <chenriquealvarenga@gmail.com>
2016-10-29 01:42:25 -0200

    integração do menu com tela de ranking e seleção de modo de jogo

--- core/src/br/cefetmg/games/screens/MenuScreen.java
+++ core/src/br/cefetmg/games/screens/MenuScreen.java
@@ -10,29 +10,51 @@ import br.cefetmg.games.minigames.util.ActualMenuScreen;
 import br.cefetmg.games.minigames.util.GameOption;
 import br.cefetmg.games.minigames.util.Score;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 import java.util.ArrayList;
+
+//imports Carlos e Bruno
+
+import com.badlogic.gdx.graphics.Texture; 
+import com.badlogic.gdx.scenes.scene2d.Actor; 
+import com.badlogic.gdx.scenes.scene2d.Stage; 
+import com.badlogic.gdx.scenes.scene2d.ui.Table; 
+import com.badlogic.gdx.scenes.scene2d.ui.Button; 
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton; 
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener; 
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable; 
 
 /**
  * Uma tela de Menu Principal do jogo.
  *
  * @author Flávio Coutinho - fegemo <coutinho@decom.cefetmg.br>
  */
-public class MenuScreen extends BaseScreen {
 
-    private static final int NUMBER_OF_TILED_BACKGROUND_TEXTURE = 7;
-    private TextureRegion background;
+public class MenuScreen extends BaseScreen {
+    
+    //****linhas apagadas by Bruno e Carlos
+    //private static final int NUMBER_OF_TILED_BACKGROUND_TEXTURE = 7;
+    //private TextureRegion background;
+    //****fim
+    
     private final Sound introMusic;
     
+    //****linhas apagadas by Bruno e Carlos
     //***Alterações para o modo survival by Lindley and Lucas Viana
-    private final Sprite normalButton, survivalButton, rankingButton;
+    //private final Sprite normalButton, survivalButton, rankingButton;
+    //****fim
     private ActualMenuScreen actualScreen;
     private final Rank rank;
     //***Fim do bloco de alterações
-
+    
+    //****Textures e stage by Bruno e Carlos
+    private TextureRegion background,backgroundRanking,buttonIniciarTexture,buttonCreditosTexture,buttonSairTexture,
+                buttonSurvivalTexture,buttonNormalTexture,buttonRankingTexture,buttonVoltarTexture; 
+    private Stage stage, stageRanking;
+    private Button buttonIniciar, buttonSair, buttonCreditos, buttonSurvival, buttonNormal,buttonRanking,buttonVoltar;
+    //****fim
+    
     /**
      * Cria uma nova tela de menu.
      *
@@ -44,21 +66,21 @@ public class MenuScreen extends BaseScreen {
         
         //Define a música tema
         introMusic = Gdx.audio.newSound(Gdx.files.internal("sounds/menu.mp3"));
-
-         //***Alterações para o modo survival by Lindley and Lucas Viana
+        
+        //****linhas apagadas by Bruno e Carlos
+        //***Alterações para o modo survival by Lindley and Lucas Viana
         //Carrega texturas para os botões do menu
-        normalButton = new Sprite(new Texture("buttons_menu/Normal.png"));
-        survivalButton = new Sprite(new Texture("buttons_menu/Survival.png"));
-        rankingButton = new Sprite(new Texture("buttons_menu/Ranking.png"));
+        //normalButton = new Sprite(new Texture("buttons_menu/Normal.png"));
+        //survivalButton = new Sprite(new Texture("buttons_menu/Survival.png"));
+        //rankingButton = new Sprite(new Texture("buttons_menu/Ranking.png"));
 
         //Define as posições dos botões
-        normalButton.setPosition(40 * viewport.getWorldWidth() / 200.0f, viewport.getWorldHeight() / 2.5f);
-        survivalButton.setPosition(25 + 80 * viewport.getWorldWidth() / 200.0f, viewport.getWorldHeight() / 2.5f);
-        rankingButton.setPosition(50 + 120 * viewport.getWorldWidth() / 200.0f, viewport.getWorldHeight() / 2.5f);
-
+        //normalButton.setPosition(40 * viewport.getWorldWidth() / 200.0f, viewport.getWorldHeight() / 2.5f);
+        //survivalButton.setPosition(25 + 80 * viewport.getWorldWidth() / 200.0f, viewport.getWorldHeight() / 2.5f);
+        //rankingButton.setPosition(50 + 120 * viewport.getWorldWidth() / 200.0f, viewport.getWorldHeight() / 2.5f);
+        //****fim
         //Inicializa tela
         actualScreen = ActualMenuScreen.MENU;
-        
         rank = new Rank();
         //***Fim do bloco de alterações
     }
@@ -69,26 +91,109 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void show() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
-
         introMusic.play();
-        // instancia a textura e a região de textura (usada para repetir)
-        background = new TextureRegion(new Texture("menu-background.png"));
-        // configura a textura para repetir caso ela ocupe menos espaço que o
-        // espaço disponível
-        background.getTexture().setWrap(
-                Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+        
+        background = new TextureRegion(new Texture("menu_background_m.jpg"));
+        backgroundRanking = new TextureRegion(new Texture("menu_background_r.jpg"));
+        
+        //****Instanciando Menu by Bruno e Carlos
+        stage = new Stage(); 
+        stageRanking = new Stage();
+        
+	// Create a table that fills the screen. Everything else will go inside this table. 
+        final Table table = new Table(); 
+        table.align(1); 
+        table.padBottom(160); 
+        table.setFillParent(true); 
+        
+        final Table tableGameMode = new Table();
+        tableGameMode.align(1); 
+        tableGameMode.padBottom(160); 
+        tableGameMode.setFillParent(true);
+        tableGameMode.setVisible(false);
 
-        // define a largura da região de desenho de forma que ela seja repetida
-        // um número de vezes igual a NUMBER_OF_TILED_BACKGROUND_TEXTURE 
-        background.setRegionWidth(
-                background.getTexture().getWidth()
-                * NUMBER_OF_TILED_BACKGROUND_TEXTURE);
-        // idem para altura, porém será repetida um número de vezes igual a 
-        // NUMBER_OF_TILED_BACKGROUND_TEXTURE * razãoDeAspecto
-        background.setRegionHeight(
-                (int) (background.getTexture().getHeight()
-                * NUMBER_OF_TILED_BACKGROUND_TEXTURE
-                / Config.DESIRED_ASPECT_RATIO));
+        buttonIniciarTexture = new TextureRegion(new Texture("buttons_menu/button_iniciar.png")); 
+        buttonIniciar = new ImageButton(new TextureRegionDrawable(buttonIniciarTexture)); 
+        table.add(buttonIniciar);
+        
+        buttonNormalTexture = new TextureRegion(new Texture("buttons_menu/Normal.png")); 
+        buttonNormal = new ImageButton(new TextureRegionDrawable(buttonNormalTexture));
+        buttonNormal.pad(80);
+        tableGameMode.add(buttonNormal);
+        
+        buttonSurvivalTexture = new TextureRegion(new Texture("buttons_menu/Survival.png")); 
+        buttonSurvival = new ImageButton(new TextureRegionDrawable(buttonSurvivalTexture));
+        buttonSurvival.pad(80);
+        tableGameMode.add(buttonSurvival);
+        
+        buttonRankingTexture = new TextureRegion(new Texture("buttons_menu/button_ranking.png")); 
+        buttonRanking = new ImageButton(new TextureRegionDrawable(buttonRankingTexture)); 
+        table.add(buttonRanking);
+
+        buttonCreditosTexture = new TextureRegion(new Texture("buttons_menu/button_creditos.png")); 
+        buttonCreditos = new ImageButton(new TextureRegionDrawable(buttonCreditosTexture)); 
+        table.add(buttonCreditos);  
+
+        buttonSairTexture = new TextureRegion(new Texture("buttons_menu/button_sair.png")); 
+        buttonSair = new ImageButton(new TextureRegionDrawable(buttonSairTexture)); 
+        table.add(buttonSair);
+        
+        buttonVoltarTexture = new TextureRegion(new Texture("buttons_menu/button_voltar.png")); 
+        buttonVoltar = new ImageButton(new TextureRegionDrawable(buttonVoltarTexture));
+        buttonVoltar.align(2);
+        
+        
+        buttonIniciar.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) { 
+                table.setVisible(false);
+                tableGameMode.setVisible(true);
+            } 
+        }); 
+        
+        buttonSurvival.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) {
+                introMusic.stop();
+                navigateToMicroGameScreen(GameOption.SURVIVAL); 
+            } 
+        });
+        
+        buttonNormal.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) {
+                introMusic.stop();
+                navigateToMicroGameScreen(GameOption.NORMAL);
+            } 
+        });
+        
+        buttonRanking.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) {
+                actualScreen = ActualMenuScreen.RANKING;
+                Gdx.input.setInputProcessor(stageRanking);
+            } 
+        });
+        
+        buttonCreditos.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) { 
+                    //TODO 
+            } 
+        }); 
+
+        buttonSair.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) { 
+                Gdx.app.exit(); 
+            } 
+        }); 
+        
+        buttonVoltar.addListener(new ChangeListener() { 
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) { 
+                actualScreen = ActualMenuScreen.MENU;
+                Gdx.input.setInputProcessor(stage);
+            } 
+        }); 
+
+        stage.addActor(table); 
+        stage.addActor(tableGameMode);
+        stageRanking.addActor(buttonVoltar);
+        Gdx.input.setInputProcessor(stage);
     }
 
     /**
@@ -96,29 +201,7 @@ public class MenuScreen extends BaseScreen {
      */
     @Override
     public void handleInput() {
-        /**
-         * Tratamento do click do mouse :: Lindley e Lucas Viana
-         */
-        Vector2 click = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-        viewport.unproject(click);        
         
-        // se qualquer interação é feita (teclado, mouse pressionado, tecla
-        // tocada), navega para a próxima tela (de jogo)
-        if (Gdx.input.justTouched()) {
-            if (actualScreen == ActualMenuScreen.MENU) {
-                if (normalButton.getBoundingRectangle().contains(click)) {
-                    introMusic.stop();
-                    navigateToMicroGameScreen(GameOption.NORMAL);
-                } else if (survivalButton.getBoundingRectangle().contains(click)) {
-                    introMusic.stop();
-                    navigateToMicroGameScreen(GameOption.SURVIVAL);
-                } else if (rankingButton.getBoundingRectangle().contains(click)) {
-                    actualScreen = ActualMenuScreen.RANKING;
-                }
-            } else {
-                actualScreen = ActualMenuScreen.MENU;
-            }
-        }
     }
     
     /**
@@ -128,8 +211,7 @@ public class MenuScreen extends BaseScreen {
      */
     @Override
     public void update(float dt) {
-        float speed = dt * 0.25f;
-        background.scroll(speed, -speed);
+
     }
 
     /**
@@ -139,12 +221,10 @@ public class MenuScreen extends BaseScreen {
     public void draw() {        
         batch.begin();
 
-        batch.draw(background, 0, 0,
-                viewport.getWorldWidth(),
-                viewport.getWorldHeight());
         //***Alterações para o modo survival by Lindley and Lucas Viana
         //Desenhar ranking ou opcaos de jogo
         if (actualScreen == ActualMenuScreen.MENU) {
+            /****linhas apagadas por Bruno e Carlos
             drawCenterAlignedText("Selecione o modo de jogo",
                     1f, viewport.getWorldHeight() * 0.35f);
 
@@ -152,14 +232,29 @@ public class MenuScreen extends BaseScreen {
             normalButton.draw(this.batch);
             survivalButton.draw(this.batch);
             rankingButton.draw(this.batch);
-
+            fim****/
+            batch.draw(background, 0, 0,
+                viewport.getWorldWidth(),
+                viewport.getWorldHeight());
+            
+            drawCenterAlignedText("Pressione qualquer tecla para jogar", 
+                1f, viewport.getWorldHeight() * 0.35f); 
+            stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f)); 
+            stage.draw(); 
+            
         } else {
+            batch.draw(backgroundRanking, 0, 0,
+                viewport.getWorldWidth(),
+                viewport.getWorldHeight());
+            
             ArrayList<Score> ranking = rank.getRanking();
             for (int i = 0; i < ranking.size(); ++i) {
                 drawCenterAlignedText(ranking.get(i).getName()
                         + " .......... " + ranking.get(i).getGames(),
                         1.0f, viewport.getWorldHeight() - 50f * (i + 1));
             }
+            buttonVoltar.draw(batch, 1);
+             
         }
         /**/
         
@@ -180,5 +275,4 @@ public class MenuScreen extends BaseScreen {
             }
         }, 0.75f);// 750ms
     }
-
 }
