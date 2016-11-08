b44e4fd8d1449b9ed9c6d6799b194d45779471c6
Carlos Henrique Alvarenga <chenriquealvarenga@gmail.com>
2016-11-08 05:03:43 -0200

    adicionando escrita no db

--- core/src/br/cefetmg/games/Rank.java
+++ core/src/br/cefetmg/games/Rank.java
@@ -1,13 +1,13 @@
 package br.cefetmg.games;
 
 import br.cefetmg.games.minigames.util.Score;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.Collections;
 import com.firebase.client.Firebase;
 import com.firebase.client.ValueEventListener;
 import com.firebase.client.DataSnapshot;
@@ -17,7 +17,7 @@ import java.util.Comparator;
 
 /**
  *
- * @author lindley
+ * @author Carlos, Bruno
  */
 public class Rank {
 
@@ -29,6 +29,11 @@ public class Rank {
         readRankDB();
     }
 
+    /**
+     * Le todas as pontuacoes do db, ordena e exibe as 10 maiores.
+     * O ranking é atualizado em tempo real sempre que há alguma
+     * mudança.
+     */
     public final void readRankDB(){
         ranking = new ArrayList<Score>();
         firebase.addValueEventListener(new ValueEventListener() {
@@ -67,51 +72,89 @@ public class Rank {
         });
     }
     
-    public final void readRankFile() {
-        ranking = new ArrayList<Score>();
-        try {
-            BufferedReader getInput = new BufferedReader(
-                    new FileReader("ranking.txt"));
-            String line;
-            while ((line = getInput.readLine()) != null) {
-                String[] parts = line.split(" ");
-                ranking.add(new Score(parts[0], Integer.parseInt(parts[1])));
-//                String s = firebase.child("AAA").toString();
-//                ranking.add(new Score(s, Integer.parseInt(parts[1])));
-            }
-            getInput.close();
-        } catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n",
-                    e.getMessage());
-        }
-    }
-
-    /**
+     /**
      *
      * @return an arrayList of rank
      */
     public ArrayList<Score> getRanking() {
         return this.ranking;
     }
-
-    public void writeRankFile(String nickname, int gamesPlayed) {
-        if (ranking.size() < 10) {
-            ranking.add(new Score(nickname, gamesPlayed));
-        } else if (ranking.get(9).getGames() < gamesPlayed) {
-            ranking.remove(9);
-            ranking.add(new Score(nickname, gamesPlayed));
-        }
-        Collections.sort(ranking);
-        try {
-            BufferedWriter getOutput = new BufferedWriter(
-                    new FileWriter("ranking.txt"));
-            for (Score s : ranking) {
-                getOutput.write(s.getName() + " " + s.getGames() + "\n");
+    
+    /**
+     *
+     * Antes de escrever no banco, verifica se o nickname ja existe. Se existir,
+     * substitui a pontuacao se o novo valor for mais alto.
+     */
+    public void writeScoreDB(String nickname, final int gamesPlayed){
+        final Firebase ref = firebase.child(nickname);
+        /**
+         * O listener abaixo serve para ler a pontuacao atual do nickname
+         * para decidir se ela sera substituida.
+         */
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists()){
+                    ref.setValue(gamesPlayed);
+                }
+                else{
+                    int current_value = dataSnapshot.getValue(Integer.class);
+                    if(current_value < gamesPlayed){
+                        ref.setValue(gamesPlayed);
+                    }
+                }
             }
-            getOutput.close();
-        } catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n",
-                    e.getMessage());
-        }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getCode());
+            }
+        });
     }
+    
+    
+// /**
+// *
+// * @author lindley
+// */
+//    public final void readRankFile() {
+//        ranking = new ArrayList<Score>();
+//        try {
+//            BufferedReader getInput = new BufferedReader(
+//                    new FileReader("ranking.txt"));
+//            String line;
+//            while ((line = getInput.readLine()) != null) {
+//                String[] parts = line.split(" ");
+//                ranking.add(new Score(parts[0], Integer.parseInt(parts[1])));
+////                String s = firebase.child("AAA").toString();
+////                ranking.add(new Score(s, Integer.parseInt(parts[1])));
+//            }
+//            getInput.close();
+//        } catch (IOException e) {
+//            System.err.printf("Erro na abertura do arquivo: %s.\n",
+//                    e.getMessage());
+//        }
+//    }
+//
+//
+//    public void writeRankFile(String nickname, int gamesPlayed) {
+//        if (ranking.size() < 10) {
+//            ranking.add(new Score(nickname, gamesPlayed));
+//        } else if (ranking.get(9).getGames() < gamesPlayed) {
+//            ranking.remove(9);
+//            ranking.add(new Score(nickname, gamesPlayed));
+//        }
+//        Collections.sort(ranking);
+//        try {
+//            BufferedWriter getOutput = new BufferedWriter(
+//                    new FileWriter("ranking.txt"));
+//            for (Score s : ranking) {
+//                getOutput.write(s.getName() + " " + s.getGames() + "\n");
+//            }
+//            getOutput.close();
+//        } catch (IOException e) {
+//            System.err.printf("Erro na abertura do arquivo: %s.\n",
+//                    e.getMessage());
+//        }
+//    }
 }
