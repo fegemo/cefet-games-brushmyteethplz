63695925ca76d8d872ca11e28053756574f76441
Carlos Henrique Alvarenga <chenriquealvarenga@gmail.com>
2016-11-08 15:51:31 -0200

    ajuste para manter somente 10 primeiros scores no db

--- core/src/br/cefetmg/games/Rank.java
+++ core/src/br/cefetmg/games/Rank.java
@@ -59,12 +59,9 @@ public class Rank {
                     }
                 });
                 
-                ranking.clear();
-                for(int i = 0; i<10; i++){
-                    ranking.add(all_scores.get(i));
-                }
+                ranking = all_scores;
             }
-
+            
             @Override
             public void onCancelled(FirebaseError firebaseError) {
                 System.out.println("The read failed: " + firebaseError.getCode());
@@ -77,19 +74,53 @@ public class Rank {
      * @return an arrayList of rank
      */
     public ArrayList<Score> getRanking() {
-        return this.ranking;
+        /*
+        Ranking pode ter mais de 10 scores devido a escritas simultaneas. Caso isso ocorra,
+        retorna só os 10 primeiros valores.
+        */
+        if(ranking.size() > 10){
+            ArrayList<Score> top10 = new ArrayList<Score>();
+            for(int i = 0; i < 10; i++){
+                top10.add(ranking.get(i));
+            }
+            return top10;
+        }
+        else
+            return this.ranking;
     }
     
     /**
      *
      * Antes de escrever no banco, verifica se o nickname ja existe. Se existir,
-     * substitui a pontuacao se o novo valor for mais alto.
+     * substitui a pontuacao se o novo valor for mais alto. Se o valor for menor que
+     * o ultimo do ranking exibido, nao adiciona no banco, independente da condicao anterior.
      */
     public void writeScoreDB(String nickname, final int gamesPlayed){
+        System.out.println(ranking.size());
+        if(ranking.size() >= 10){
+            /*
+            Se nova pontuacao for menor que a decima do ranking, nao faz nada.
+            */
+            if(gamesPlayed <= ranking.get(ranking.size()-1).getGames()){
+                return;
+            }
+            else{
+                /*
+                Remove menor valor (pode remover mais de um valor, caso o ranking esteja com tamanho maior que 10
+                devido a escritas simultaneas).
+                */
+                while(ranking.size() > 9){
+                    Firebase ref = firebase.child(ranking.get(ranking.size()-1).getName());
+                    ref.removeValue();
+                    ranking.remove(ranking.size()-1);
+                }
+            }
+        }
         final Firebase ref = firebase.child(nickname);
         /**
-         * O listener abaixo serve para ler a pontuacao atual do nickname
-         * para decidir se ela sera substituida.
+         * O listener abaixo le a pontuacao atual do nickname
+         * para decidir se ela sera substituida. Se o nickname ainda nao
+         * existe no ranking, ele é adicionado.
          */
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
